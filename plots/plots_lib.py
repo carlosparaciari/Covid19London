5a9a16cc8ec43784c2180093834ea648cbb3308e
Carlo Sparaciari <carlo.sparaciari@gmail.com>
2020-04-15 22:22:24 +0100

    View for single cumulative plot created

--- plots/plots_lib.py
+++ plots/plots_lib.py
@@ -3,6 +3,7 @@
 import xlrd
 import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 from math import floor
 
@@ -11,7 +12,11 @@ def xldate_to_str(date,datemode):
     date_str = datetime_obj.strftime("%d-%b")
     return date_str
 
-def increments(cases_array, population, relative_population=1e5):
+def increments(cases_array):
+	increment_array = np.insert(np.diff(cases_array), 0, cases_array[0])
+	return list(increment_array)
+
+def relative_cases_array(cases_array, population, relative_population=1e5):
 	return [relative_population*daily_cases/population for daily_cases in cases_array]
 
 def tick_dates(dates,number_shown=5):
@@ -19,9 +24,9 @@ def tick_dates(dates,number_shown=5):
     ticks = [dates[n*interval] for n in range(number_shown+1)]
     return ticks
 
-def cumulative_plot_abs(dates_str,cases,increment,area):
-    fig = plt.figure()
-    plt.plot(dates_str,cases) # cumulative in time
+def cumulative_plot_abs(dates_str,cases,increment,area,figure_size=(7.5,5.5)):
+    fig = plt.figure(figsize=figure_size)
+    plt.plot(dates_str,cases,linewidth=2) # cumulative in time
     plt.bar(dates_str,increment,color='g') # increment in bars
     plt.title(area+' borough (cumulative cases)')
     plt.xlabel('Days')
--- plots/views.py
+++ plots/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 import io
 import urllib, base64
 from .plots_lib import increments, cumulative_plot_abs
 from .models import Borough, Dates
 
 def index(request):
+	return HttpResponseRedirect(reverse('plots:cumulative_single', args=('London',)))
+
+def cumul_abs(request,borough_name):
+
+	# Make the dropdown menu
+	menu_items = [entry.name for entry in Borough.objects.all()]
 
 	# Get relevant borough and dates
-	b = Borough.objects.get(name__exact='Southwark')
+	b = get_object_or_404(Borough, name__exact=borough_name)
 	d = Dates.objects.get()
 
 	# Get the daily increments for the borough 
-	b_increments = increments(b.cumulative_array, b.population)
+	b_increments = increments(b.cumulative_array)
 
 	# Plot of cumulative cases
 	cumul_abs = cumulative_plot_abs(d.dates_array, b.cumulative_array, b_increments, b.name)
@@ -23,4 +31,6 @@ def index(request):
 	string = base64.b64encode(buf.read())
 	uri = urllib.parse.quote(string)
 
-	return render(request, 'index.html', {'data':uri})
+	context = {'data':uri,'items':menu_items, 'current':b.name}
+	
+	return render(request, 'plots/cumulative_abs.html', context)
