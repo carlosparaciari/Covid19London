47a5426722425600e4acd7a596e103f41dbd9e17
Carlo Sparaciari <carlo.sparaciari@gmail.com>
2020-05-01 17:16:11 +0100

    New date form to select daily cases

--- plots/forms.py
+++ plots/forms.py
@@ -1,4 +1,21 @@
 from django import forms
 
+from datetime import date
+
 class BoroughChecklist(forms.Form):
-    show_borough = forms.BooleanField()
+	show_borough = forms.BooleanField()
+
+class IncrementData(forms.Form):
+
+	date = forms.DateField(widget = forms.SelectDateWidget( years=(2020,) ))
+
+	def __init__(self,*args,**kwargs):
+
+		try:
+			self.date_init = kwargs.pop('date_value')
+		except KeyError:
+			self.date_init = date.today()
+
+		super(IncrementData,self).__init__(*args,**kwargs)
+
+		self.fields['date'].initial = self.date_init
