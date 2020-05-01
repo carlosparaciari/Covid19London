2cbc27492fdaf6855b6d6df94166f1236733d022
Carlo Sparaciari <carlo.sparaciari@gmail.com>
2020-05-01 17:18:05 +0100

    view for cumulative abs updated to let the user select daily cases

--- plots/views.py
+++ plots/views.py
@@ -1,39 +1,71 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+
 import io
-import urllib, base64
+import urllib
+import base64
 import numpy as np
+from datetime import datetime
+
 from .plots_lib import increments, relative_cases_array, cumulative_plot_abs, cumulative_plot_rel, prepare_checklist_boroughs
 from .models import Borough, Dates
+from .forms import IncrementData
 
 def index(request):
 	return HttpResponseRedirect(reverse('plots:cumulative_single', args=('London',)))
 
 def cumul_abs(request,borough_name):
 
+	# Get dates array from database
+	d = Dates.objects.get()
+	obj_dates_array = d.get_dates()
+	dates_array = np.array([obj_date.strftime('%d %b') for obj_date in obj_dates_array])
+
+	# Get the latest date the database was updated
+	last_update = obj_dates_array[-1]
+	last_update_str = last_update.strftime('%d %B')
+
+	# Get the date form (if it was filled)
+	if request.method == 'POST':
+		response_post = request.POST
+		response_dict = response_post.dict()
+
+		year_val = int(response_dict['date_year'])
+		month_val = int(response_dict['date_month'])
+		day_val = int(response_dict['date_day'])
+
+		date_val = datetime(year_val, month_val, day_val)
+		date_val_str = date_val.strftime('%d %b')
+
+		if not date_val_str in dates_array:
+			date_val = last_update
+			date_val_str = date_val.strftime('%d %b')
+	else:
+		date_val = last_update
+		date_val_str = date_val.strftime('%d %b')
+
 	# Make the dropdown menu
 	menu_items = [entry for entry in Borough.objects.values_list('name', flat=True)]
 
 	# Get relevant borough and dates
 	b = get_object_or_404(Borough, name__exact=borough_name)
-	d = Dates.objects.get()
-
-	# Get the dates and the date of latest update
-	d_dates_array = np.array(d.dates_array)
-	last_update = (d_dates_array)[-1]
 
 	# Get the cumulative array and the daily increments for the borough
 	b_cumulative_array = np.array(b.cumulative_array)
 	b_increments = increments(b_cumulative_array)
 
+	# Find cases for date requested
+	date_val_str = date_val.strftime('%d %b')
+	date_index = dates_array==date_val_str
+
 	# Daily information
-	daily_total = (b_cumulative_array)[-1]
-	daily_increment = b_increments[-1]
+	daily_total = b_cumulative_array[date_index][0]
+	daily_increment = b_increments[date_index][0]
 	daily_percentage = "{:.1f}".format(100*daily_increment/(daily_total-daily_increment))
 
 	# Plot of cumulative cases
-	cumul_abs = cumulative_plot_abs(d_dates_array, b_cumulative_array, b_increments, b.name)
+	cumul_abs = cumulative_plot_abs(dates_array, b_cumulative_array, b_increments, b.name)
 
 	# Save plot into buffer and convert to be able to visualise it
 	buf = io.BytesIO()
@@ -43,14 +75,15 @@ def cumul_abs(request,borough_name):
 	uri = urllib.parse.quote(string)
 
 	# Data to pass to the html page
-	context = {'data':uri,
-			   'items':menu_items,
-			   'current':b.name,
-			   'date':last_update,
-			   'daily_tot':daily_total,
-			   'daily_inc':daily_increment,
-			   'daily_per':daily_percentage
-			  }
+	context = {}
+	context['data']=uri
+	context['items']=menu_items
+	context['current']=b.name
+	context['date']=last_update_str
+	context['date_form'] = IncrementData(date_value=date_val) 
+	context['daily_tot']=daily_total
+	context['daily_inc']=daily_increment
+	context['daily_per']=daily_percentage
 	
 	return render(request, 'plots/cumulative_abs.html', context)
 
@@ -104,7 +137,9 @@ def cumul_rel(request):
 
 	# Get the date of latest update
 	d = Dates.objects.get()
-	last_update = (d.dates_array)[-1]
+	obj_dates_array = d.get_dates()
+	last_update = obj_dates_array[-1]
+	last_update_str = last_update.strftime('%d %B')
 
 	# Save plot into buffer and convert to be able to visualise it
 	buf = io.BytesIO()
@@ -115,7 +150,7 @@ def cumul_rel(request):
 
 	context = {'data':uri,
 			   'items':checkbox_items,
-			   'date':last_update
+			   'date':last_update_str
 			  }
 
 	return render(request, 'plots/cumulative_rel.html', context)
