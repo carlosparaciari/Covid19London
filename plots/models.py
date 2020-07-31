f5d3a1b7260f6db00108b194f8f4efc2e88738f6
Carlo Sparaciari <carlo.sparaciari@gmail.com>
2020-07-31 14:09:36 +0200

    Abstract class for regional data model

--- plots/models.py
+++ plots/models.py
@@ -4,14 +4,29 @@ from django.db import models
 from datetime import datetime
 import numpy as np
 
-# Model for saving cumulative cases of London's boroughs
-class Borough(models.Model):
+# Abstract model for data of regional areas
+class RegionalArea(models.Model):
 	name = models.CharField(max_length=200)
 	population = models.IntegerField(default=0)
 	cumulative_array = ArrayField(models.IntegerField())
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+        abstract = True
+
+# Model for saving cumulative cases of London's boroughs
+class Borough(RegionalArea):
+
+	class Meta(RegionalArea.Meta):
+        db_table = 'London_boroughs'
+
+# Model for saving cumulative cases of Italian province
+class Province(RegionalArea):
+
+	class Meta(RegionalArea.Meta):
+        db_table = 'Italy_provinces'
 
 # Model to save the dates since first infection
 class Dates(models.Model):
