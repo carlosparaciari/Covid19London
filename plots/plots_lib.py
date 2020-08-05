0684acd4c5bde53d5eb3afbd98b5b9eab4579d23
Carlo Sparaciari <carlo.sparaciari@gmail.com>
2020-08-05 12:17:38 +0200

    views have been simplified

--- plots/plots_lib.py
+++ plots/plots_lib.py
@@ -1,10 +1,11 @@
 # Library for plot tasks and views
 
-import matplotlib.pyplot as plt
 import io
 import urllib
 import base64
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib._color_data as mcd
 
 from django.http import Http404
 from django.shortcuts import get_object_or_404
@@ -13,6 +14,158 @@ from math import ceil
 from datetime import datetime
 
 from .forms import DailyCasesForm
+
+## CONTEXT FUNCTIONS
+
+# This function prepare the context for the absolute plots (for both London and Italy)
+def cumulative_cases(request,model_date,model_region,regional_name):
+
+    # Get dates array from database and last date of update
+    d = model_date.objects.get()
+    dates_array = d.get_dates('%d %b')
+    last_update = d.get_single_date_str(-1,'%d %B')
+
+    # Set the date the user is interested in (default is latest day)
+    date_val = d.get_single_date(-1)
+
+    # If the user has provided a date
+    if request.method == 'GET' and 'date' in request.GET:
+        response = request.GET
+        response_date = response.get('date')
+
+        try:
+            date_object = datetime.strptime(response_date, '%Y-%m-%d')
+        except ValueError:
+            pass
+        else:
+            date_requested = date_object.strftime('%d %b')
+            if date_requested in dates_array:
+                date_val = date_object
+
+    date_val_str = date_val.strftime('%d %b')
+
+    # Make the dropdown menu
+    full_list = [entry for entry in model_region.objects.values_list('name', flat=True)]
+
+    try:
+        full_list.remove('London')
+        sort_list = np.sort(full_list)
+        menu_items = np.append(sort_list,'London')
+    except ValueError:
+        menu_items = np.sort(full_list)
+
+    # Get relevant region and dates
+    b = get_object_or_404(model_region, name__exact=regional_name)
+
+    # Get the cumulative array and the daily increments for the borough
+    b_cumulative_array = np.array(b.cumulative_array)
+    b_increments = increments(b_cumulative_array)
+
+    # Find cases for date requested
+    date_index = dates_array==date_val_str
+
+    # Daily information
+    daily_total = b_cumulative_array[date_index][0]
+    daily_increment = b_increments[date_index][0]
+    daily_percentage = "{:.1f}".format(100*daily_increment/(daily_total-daily_increment))
+
+    # Plot of cumulative cases
+    cumul_abs = cumulative_plot_abs(dates_array, b_cumulative_array, b_increments, b.name)
+
+    # Save plot into buffer and convert to be able to visualise it
+    buf = io.BytesIO()
+    cumul_abs.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+
+    # Data to pass to the html page
+    context = {}
+    context['data']=uri
+    context['items']=menu_items
+    context['current']=b.name
+    context['date']=last_update
+    context['date_form']=DailyCasesForm(initial={'date': date_val}) 
+    context['daily_tot']=daily_total
+    context['daily_inc']=daily_increment
+    context['daily_per']=daily_percentage
+
+    return context
+
+# This function prepare the context for the relative plots (currently only for London)
+def relative_cases(request,model_date,model_region):
+
+    # Prepare list of borough to be plotted
+    if request.method == 'POST':
+        response_post = request.POST
+        response_dict = response_post.dict()
+
+        borough_names = model_region.objects.values_list('name',flat=True)
+        borough_names = borough_names.exclude(name='London')
+        checkbox_items = [(name, name in response_dict) for name in borough_names]
+    else:
+        queryset_name_cases = model_region.objects.values_list('name','cumulative_array')
+        queryset_name_cases = queryset_name_cases.exclude(name='London')
+        checkbox_items = prepare_checklist_boroughs(queryset_name_cases)
+
+    # Order the checkbox list
+    checkbox_items.sort(key=lambda tup: tup[0])
+    borough_list, bool_list = [*zip(*checkbox_items)]
+    borough_array = np.array(borough_list)
+    bool_array = np.array(bool_list)
+
+    # Predefine colour palette
+    col_array = np.array([col for n,col in enumerate(mcd.XKCD_COLORS.values()) if n < len(checkbox_items)])
+    col_list = col_array[bool_array]
+
+    # Make a list of the selected boroughs plus London
+    area_list = borough_array[bool_array]
+    area_list = np.append(area_list,'London')
+
+    # Extract the relative data for each borough, and only keep cases >= 1
+    cases_rel_list = []
+    length_arrays = []
+
+    for borough_name in area_list:
+        b = model_region.objects.get(name__exact=borough_name)
+        cases_rel = relative_cases_array(b.cumulative_array, b.population)
+        relevant_cases = cases_rel >= 1
+        cases_rel_list.append(cases_rel[relevant_cases]) # Get the relevant cases
+        length_arrays.append(np.sum(relevant_cases)) # Check the lenght of the above array
+
+    # Compute the maximum length of the data when relative cases >= 1
+    max_length = max(length_arrays)
+
+    # Pad each array to get the same length
+    cases_pad_list = []
+
+    for cases_rel in cases_rel_list:
+        padding_length = max_length-cases_rel.size
+        pad_cumul_rel = np.pad(cases_rel, (0,padding_length), 'constant', constant_values=np.nan)
+        cases_pad_list.append(pad_cumul_rel)
+
+    # Prepare the data to pass the plot function
+    days_since = range(max_length) # Days since 1st relative case
+
+    cumul_rel = cumulative_plot_rel(days_since,cases_pad_list,area_list,col_list)
+
+    # Get the date of latest update
+    d = model_date.objects.get()
+    last_update = d.get_single_date_str(-1,'%d %B')
+
+    # Save plot into buffer and convert to be able to visualise it
+    buf = io.BytesIO()
+    cumul_rel.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+
+    context = {}
+    context['data']=uri
+    context['items']=checkbox_items
+    context['date']=last_update
+
+    return context
 
 # This function prepare the context for the trend plots (for both London and Italy)
 def regions_trends(request,model_date,model_region,regional_name,pop_rel=1e5):
@@ -118,80 +271,7 @@ def regions_trends(request,model_date,model_region,regional_name,pop_rel=1e5):
 
     return context
 
-# This function prepare the context for the absolute plots (for both London and Italy)
-def cumulative_cases(request,model_date,model_region,regional_name):
-
-    # Get dates array from database and last date of update
-    d = model_date.objects.get()
-    dates_array = d.get_dates('%d %b')
-    last_update = d.get_single_date_str(-1,'%d %B')
-
-    # Set the date the user is interested in (default is latest day)
-    date_val = d.get_single_date(-1)
-
-    # If the user has provided a date
-    if request.method == 'GET' and 'date' in request.GET:
-        response = request.GET
-        response_date = response.get('date')
-
-        try:
-            date_object = datetime.strptime(response_date, '%Y-%m-%d')
-        except ValueError:
-            pass
-        else:
-            date_requested = date_object.strftime('%d %b')
-            if date_requested in dates_array:
-                date_val = date_object
-
-    date_val_str = date_val.strftime('%d %b')
-
-    # Make the dropdown menu
-    full_list = [entry for entry in model_region.objects.values_list('name', flat=True)]
-
-    try:
-        full_list.remove('London')
-        sort_list = np.sort(full_list)
-        menu_items = np.append(sort_list,'London')
-    except ValueError:
-        menu_items = np.sort(full_list)
-
-    # Get relevant region and dates
-    b = get_object_or_404(model_region, name__exact=regional_name)
-
-    # Get the cumulative array and the daily increments for the borough
-    b_cumulative_array = np.array(b.cumulative_array)
-    b_increments = increments(b_cumulative_array)
-
-    # Find cases for date requested
-    date_index = dates_array==date_val_str
-
-    # Daily information
-    daily_total = b_cumulative_array[date_index][0]
-    daily_increment = b_increments[date_index][0]
-    daily_percentage = "{:.1f}".format(100*daily_increment/(daily_total-daily_increment))
-
-    # Plot of cumulative cases
-    cumul_abs = cumulative_plot_abs(dates_array, b_cumulative_array, b_increments, b.name)
-
-    # Save plot into buffer and convert to be able to visualise it
-    buf = io.BytesIO()
-    cumul_abs.savefig(buf,format='png')
-    buf.seek(0)
-    string = base64.b64encode(buf.read())
-    uri = urllib.parse.quote(string)
-
-    # Data to pass to the html page
-    context = {}
-    context['data']=uri
-    context['items']=menu_items
-    context['current']=b.name
-    context['date']=last_update
-    context['date_form']=DailyCasesForm(initial={'date': date_val}) 
-    context['daily_tot']=daily_total
-    context['daily_inc']=daily_increment
-    context['daily_per']=daily_percentage
-
-    return context
+## AUXILIARY FUNCTIONS
 
 # Provide the daily increments from the array of cumulative cases
 def increments(cases_array):
@@ -259,60 +339,25 @@ def extremal_regions(point_list,order_measure,n=2):
     
     return areas_list, cases_list, incr_list
 
-# Plot each regional area as a point  on a 2D plot with
-# x-axis : the total number of cases reported to date
-# y-axis : the increse in the number of cases reported during the week
-def plot_trends(region_data,all_data,average_data,highlights):
-    
-    # Unpacking data
-    cases_list, incr_list = all_data
-    average_cases, average_incr = average_data
+# Prepare the list of the most affected borough in london
+#
+# INPUT:
+#       - queryset : QuerySet object with name of borough and array of cases
+#       - treshold : number of borough to check (compare)
+#
+# OUTPUT:
+#       - checkbox_items : list of tuples, with name of borough and boolean for check (compare)
+#
+def prepare_checklist_boroughs(queryset,treshold=3):
+    affected_boroughs = [(name,cases[-1]) for name,cases in queryset]
+    affected_boroughs = sorted(affected_boroughs, key=lambda tup: tup[1],reverse = True)
+    most_affected_boroughs = [affected_boroughs[iterator][0] for iterator in range(1,treshold+1)]
 
-    plt.rc('xtick',labelsize=12)
-    plt.rc('ytick',labelsize=12)
+    checkbox_items = [(name, name in most_affected_boroughs) for name,_ in queryset]
 
-    fig = plt.figure()
-    
-    # Plot all region for reference
-    plt.scatter(*all_data, c='lightgray',marker='.')
+    return checkbox_items
 
-    # Divide the plot between high and low increments (compared to average)
-    xmax = max(cases_list)
-    xmin = min(cases_list)
-    plt.hlines(average_incr, xmin, xmax, linestyles='--',linewidth=.6)
-
-    # Divide the plot between high and low cases (compared to average)
-    ymax = max(incr_list)
-    ymin = min(incr_list)
-    plt.vlines(average_cases, ymin, ymax, linestyles='--',linewidth=.6)
-
-    # Plot the average point
-    plt.scatter([average_cases], [average_incr], c='black',marker='.')
-
-    # Plot the highlighted regions in the different quadrants
-    colours = ('red','orange','green','orange')
-    
-    for val, col in zip(highlights,colours):
-        plt.scatter(val[1], val[2], c=col,marker='.')
-    
-    # Annotate the highlighted points with the corresponding regional name
-    delta_y = 0.01*(ymax-ymin)
-
-    for areas, cases, incrs in highlights:
-        for text, x, y in zip(areas, cases, incrs):
-            plt.annotate(text, (x,y+delta_y),size=14)        
-    
-    # Highlight the region of interest
-    plt.scatter(*region_data,marker="X",zorder=10)
-    
-    plt.box(on=None)
-    
-    plt.xlabel('Total number of cases',size=14)
-    plt.ylabel('Weekly increase in cases',size=14)
-    
-    plt.close()
-    
-    return fig
+## PLOT FUNCTIONS
 
 # Plot of cumulative cases plus increments
 def cumulative_plot_abs(dates_str,cases,increment,area):
@@ -396,20 +441,57 @@ def cumulative_plot_rel(dates_str,cases_rel_list,area_list,col_list,pop_rel=1e5)
     
     return fig
 
-# Prepare the list of the most affected borough in london
-#
-# INPUT:
-#       - queryset : QuerySet object with name of borough and array of cases
-#       - treshold : number of borough to check (compare)
-#
-# OUTPUT:
-#       - checkbox_items : list of tuples, with name of borough and boolean for check (compare)
-#
-def prepare_checklist_boroughs(queryset,treshold=3):
-    affected_boroughs = [(name,cases[-1]) for name,cases in queryset]
-    affected_boroughs = sorted(affected_boroughs, key=lambda tup: tup[1],reverse = True)
-    most_affected_boroughs = [affected_boroughs[iterator][0] for iterator in range(1,treshold+1)]
+# Plot each regional area as a point  on a 2D plot with
+# x-axis : the total number of cases reported to date
+# y-axis : the increse in the number of cases reported during the week
+def plot_trends(region_data,all_data,average_data,highlights):
+    
+    # Unpacking data
+    cases_list, incr_list = all_data
+    average_cases, average_incr = average_data
 
-    checkbox_items = [(name, name in most_affected_boroughs) for name,_ in queryset]
+    plt.rc('xtick',labelsize=12)
+    plt.rc('ytick',labelsize=12)
 
-    return checkbox_items
+    fig = plt.figure()
+    
+    # Plot all region for reference
+    plt.scatter(*all_data, c='lightgray',marker='.')
+
+    # Divide the plot between high and low increments (compared to average)
+    xmax = max(cases_list)
+    xmin = min(cases_list)
+    plt.hlines(average_incr, xmin, xmax, linestyles='--',linewidth=.6)
+
+    # Divide the plot between high and low cases (compared to average)
+    ymax = max(incr_list)
+    ymin = min(incr_list)
+    plt.vlines(average_cases, ymin, ymax, linestyles='--',linewidth=.6)
+
+    # Plot the average point
+    plt.scatter([average_cases], [average_incr], c='black',marker='.')
+
+    # Plot the highlighted regions in the different quadrants
+    colours = ('red','orange','green','orange')
+    
+    for val, col in zip(highlights,colours):
+        plt.scatter(val[1], val[2], c=col,marker='.')
+    
+    # Annotate the highlighted points with the corresponding regional name
+    delta_y = 0.01*(ymax-ymin)
+
+    for areas, cases, incrs in highlights:
+        for text, x, y in zip(areas, cases, incrs):
+            plt.annotate(text, (x,y+delta_y),size=14)        
+    
+    # Highlight the region of interest
+    plt.scatter(*region_data,marker="X",zorder=10)
+    
+    plt.box(on=None)
+    
+    plt.xlabel('Total number of cases',size=14)
+    plt.ylabel('Weekly increase in cases',size=14)
+    
+    plt.close()
+    
+    return fig
--- plots/views.py
+++ plots/views.py
@@ -1,110 +1,33 @@
-from django.http import HttpResponseRedirect, HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
 from django.urls import reverse
 
-import io
-import urllib
-import base64
-import numpy as np
-from datetime import datetime
-import matplotlib._color_data as mcd
-
-from .plots_lib import increments, relative_cases_array, regions_trends, cumulative_cases, cumulative_plot_abs, cumulative_plot_rel, prepare_checklist_boroughs
+from .plots_lib import cumulative_cases, relative_cases, regions_trends
 from .models import Borough, Province, LondonDate, ItalyDate
 
 def index(request):
 	return HttpResponseRedirect(reverse('plots:cumulative_single', args=('London',)))
 
-def cumul_abs(request,borough_name):
+## LONDON VIEWS
 
+def cumul_abs(request,borough_name):
 	context = cumulative_cases(request,LondonDate,Borough,borough_name)
 	return render(request, 'plots/cumulative_abs.html', context)
 
-def trends(request,borough_name):
+def cumul_rel(request):
+	context = relative_cases(request,LondonDate,Borough)
+	return render(request, 'plots/cumulative_rel.html', context)
 
+def trends(request,borough_name):
 	context = regions_trends(request,LondonDate,Borough,borough_name)
 	return render(request, 'plots/trends.html', context)
 
-def cumul_ita(request,province_name):
+## ITALY VIEWS
 
+def cumul_ita(request,province_name):
 	context = cumulative_cases(request,ItalyDate,Province,province_name)
 	return render(request, 'plots/cumulative_italy.html', context)
 
 def trends_ita(request,province_name):
-
 	context = regions_trends(request,ItalyDate,Province,province_name)
 	return render(request, 'plots/trends_italy.html', context)
-
-def cumul_rel(request):
-
-	# Prepare list of borough to be plotted
-	if request.method == 'POST':
-		response_post = request.POST
-		response_dict = response_post.dict()
-
-		borough_names = Borough.objects.values_list('name',flat=True)
-		borough_names = borough_names.exclude(name='London')
-		checkbox_items = [(name, name in response_dict) for name in borough_names]
-	else:
-		queryset_name_cases = Borough.objects.values_list('name','cumulative_array')
-		queryset_name_cases = queryset_name_cases.exclude(name='London')
-		checkbox_items = prepare_checklist_boroughs(queryset_name_cases)
-
-	# Order the checkbox list
-	checkbox_items.sort(key=lambda tup: tup[0])
-	borough_list, bool_list = [*zip(*checkbox_items)]
-	borough_array = np.array(borough_list)
-	bool_array = np.array(bool_list)
-
-	# Predefine colour palette
-	col_array = np.array([col for n,col in enumerate(mcd.XKCD_COLORS.values()) if n < len(checkbox_items)])
-	col_list = col_array[bool_array]
-
-	# Make a list of the selected boroughs plus London
-	area_list = borough_array[bool_array]
-	area_list = np.append(area_list,'London')
-
-	# Extract the relative data for each borough, and only keep cases >= 1
-	cases_rel_list = []
-	length_arrays = []
-
-	for borough_name in area_list:
-		b = Borough.objects.get(name__exact=borough_name)
-		cases_rel = relative_cases_array(b.cumulative_array, b.population)
-		relevant_cases = cases_rel >= 1
-		cases_rel_list.append(cases_rel[relevant_cases]) # Get the relevant cases
-		length_arrays.append(np.sum(relevant_cases)) # Check the lenght of the above array
-
-	# Compute the maximum length of the data when relative cases >= 1
-	max_length = max(length_arrays)
-
-	# Pad each array to get the same length
-	cases_pad_list = []
-
-	for cases_rel in cases_rel_list:
-	    padding_length = max_length-cases_rel.size
-	    pad_cumul_rel = np.pad(cases_rel, (0,padding_length), 'constant', constant_values=np.nan)
-	    cases_pad_list.append(pad_cumul_rel)
-
-	# Prepare the data to pass the plot function
-	days_since = range(max_length) # Days since 1st relative case
-
-	cumul_rel = cumulative_plot_rel(days_since,cases_pad_list,area_list,col_list)
-
-	# Get the date of latest update
-	d = LondonDate.objects.get()
-	last_update = d.get_single_date_str(-1,'%d %B')
-
-	# Save plot into buffer and convert to be able to visualise it
-	buf = io.BytesIO()
-	cumul_rel.savefig(buf,format='png')
-	buf.seek(0)
-	string = base64.b64encode(buf.read())
-	uri = urllib.parse.quote(string)
-
-	context = {}
-	context['data']=uri
-	context['items']=checkbox_items
-	context['date']=last_update
-
-	return render(request, 'plots/cumulative_rel.html', context)
