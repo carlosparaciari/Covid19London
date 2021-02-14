10f690b90c1615c6290d1ce07ccf5b9c311d7659
Carlo Sparaciari <carlo.sparaciari@gmail.com>
2021-02-14 20:21:08 +0000

    test fixed

--- plots/tests.py
+++ plots/tests.py
@@ -11,11 +11,12 @@ class SimpleMovingAverageTest(TestCase):
 		cls.exp_array = np.array([4.,15./4.,24./5.,21./4.,19./3.])
 		cls.test_period = 5
 		cls.wrong_period = 4
+		cls.stopping = 0
 
 	def test_exception_with_even_period(self):
 		self.assertRaises(ValueError, simple_moving_average, self.test_array, period=self.wrong_period)
 
 	def test_working_example(self):
-		obt_array = simple_moving_average(self.test_array, period=self.test_period,stopping=0)
+		obt_array = simple_moving_average(self.test_array, period=self.test_period,stopping=cls.stopping)
 		np.testing.assert_array_equal(obt_array, self.exp_array,
 									  err_msg='simple_moving_average does not work as expected')
